7cf543f266381f4289d9a613e0711191c77cfbc0
Turhan Can Kargın <turhancan.kargin@gmail.com>
2022-12-12 16:11:15 +0100

    viz for first week

---
+++ 1.Localization/Learning/localization_1D.py
@@ -0,0 +1,84 @@
+from __future__ import print_function, division
+import random
+import matplotlib.pyplot as plt
+import matplotlib.animation as animation
+from matplotlib.colors import from_levels_and_colors
+
+#Number of grid cells
+n_cells = 25
+
+#Probability of hitting the target
+p_hit = 0.6
+p_miss = 0.2
+p_exact = 0.8
+p_undershoot = 0.1
+p_overshoot = 0.1
+
+#Start with uniform probability distribution i.e. the robot could be anywhere
+p = [1/n_cells for _ in range(n_cells)]
+
+#Create the world
+world = [1 if random.random() > 0.8 else 0 for _ in range(n_cells)]
+
+#Create the color map
+colors = ["white", "black"]
+levels = [0, 1, 2]
+cmap, norm = from_levels_and_colors(levels, colors)
+
+def sense(p, Z):
+    '''update probability distribution based on measurement'''
+    q = []
+    for i in range(n_cells):
+        hit = (world[i] == Z)
+        q.append(p[i]*(hit*p_hit + (1 - hit)*p_miss))
+    norm = sum(q)
+    q = [q[i]/norm for i in range(n_cells)]
+    return q
+
+def move(p, U):
+    '''update probability distribution based on movement'''
+    q = []
+    for i in range(n_cells):
+        s = p_exact*p[(i-U)%len(p)]
+        s += p_overshoot*p[(i-U-1)%len(p)]
+        s += p_undershoot*p[(i-U+1)%len(p)]
+        q.append(s)
+    return q
+
+#Create the animation
+fig, (prob_ax, world_ax) = plt.subplots(
+    nrows=2,
+    ncols=1,
+    gridspec_kw={'height_ratios' : [10, 1]},
+    sharex=True)
+world_ax.imshow([world, world], cmap=cmap, norm=norm, interpolation=None)
+world_ax.get_yaxis().set_ticks([])
+x = [i for i in range(n_cells)]
+rects = prob_ax.bar(x, p)
+line, = world_ax.plot(0, 1, 'r.')
+
+def update(i):
+    '''update animation frame'''
+    global p
+    p = sense(p, world[i%100])
+    #print(p)
+    p = move(p, 1)
+    #print(p)
+    for rect, h in zip(rects, p):
+        rect.set_height(h)
+    line.set_xdata((i+1)%n_cells)
+    # input() # uncomment to pause at each step
+    return rects, line,
+
+def init(): 
+    '''initialize animation'''
+    prob_ax.set_xlim(0, n_cells-1)
+    prob_ax.set_ylim(0, 1)
+    world_ax.set_xlabel("Position")
+    prob_ax.set_ylabel("Probability")
+    prob_ax.set_title("Histogram Localization With Measurements")
+    return rects, line,
+
+anim = animation.FuncAnimation(fig, update, n_cells, interval=50, init_func=init)
+plt.show()
+#anim.save("only_move.gif", writer="imagemagick")
---
+++ 1.Localization/Problem-Set/localization_2D.py
@@ -0,0 +1,117 @@
+from __future__ import print_function, division
+import random
+import matplotlib.pyplot as plt
+import matplotlib.animation as animation
+
+#Number of grid cells
+m, n = 50, 50
+
+#Probability of hitting the target
+p = [[1/(m*n) for _ in range(n)] for _ in range(m)]
+world = [[1 if random.random() > 0.2 else 0 for _ in range(n)] for _ in range(m)]
+movements = [[0, 0],
+             [0, 1],
+             [1, 0],
+             [-1, 0],
+             [0, -1]]
+
+sensor_right = 0.8
+sensor_wrong = 1 - sensor_right
+p_move = 0.7
+p_stay = 1 - p_move
+
+def sense(p, Z):
+    '''It takes the probability distribution of the robot's position, and the measurement, and returns the
+    probability distribution of the robot's position after the measurement
+    
+    Parameters
+    ----------
+    p
+        the prior distribution
+    Z
+        the measurement
+    
+    Returns
+    -------
+        The probability of the robot being in a given cell, given the measurement.
+    
+    '''
+    q = [[0 for _ in range(n)] for _ in range(m)]
+    norm = 0
+    for i in range(m):
+        for j in range(n):
+            hit = (world[i][j] == Z)
+            q[i][j] = p[i][j]*(hit*sensor_right + (1 - hit)*sensor_wrong)
+            norm += q[i][j]
+    q = [[q[i][j]/norm for j in range(n)] for i in range(m)]
+    return q
+
+def move(p, U):
+    '''It takes a probability distribution p and a motion vector U, and returns a new probability
+    distribution q that is the result of moving p by U
+    
+    Parameters
+    ----------
+    p
+        the probability distribution of the robot's location
+    U
+        the action, which is a vector of two elements, the first element is the number of steps to move up,
+    the second element is the number of steps to move right.
+    
+    Returns
+    -------
+        The probability of the robot being in a given cell after it has moved.
+    
+    '''
+    q = [[0 for _ in range(n)] for _ in range(m)]
+    for i in range(m):
+        for j in range(n):
+            q[i][j] = p_move*p[(i-U[0])%m][(j-U[1])%n] + p_stay*p[i][j]
+    return q
+
+#Create the animation
+fig, ax = plt.subplots()
+grid = ax.imshow(p, cmap="GnBu")
+line, = ax.plot([], [], 'r.')
+
+i = m//2
+j = n//2
+
+def animate(_):
+    '''It takes the current state of the robot, and updates it based on the current world and the robot's
+    current position
+    
+    Parameters
+    ----------
+    _
+        This is a dummy variable that is required by the FuncAnimation function.
+    
+    Returns
+    -------
+        grid, line,
+    
+    '''
+    global p, i, j
+    U = random.choice(movements)
+    p = sense(p, world[i][j])
+    p = move(p, U)
+    i = (i + U[0])%m
+    j = (j + U[1])%n
+    grid.set_data(p)
+    smallest = min(min(row) for row in p)
+    largest = max(max(row) for row in p)
+    grid.set_clim(vmin=smallest, vmax=largest)
+    line.set_xdata(j)
+    line.set_ydata(i)
+    return grid, line,
+
+def init(): 
+    '''initialize animation'''
+    ax.set_xlabel("Position X")
+    ax.set_ylabel("Position Y")
+    ax.set_title("Histogram Localization 2D")
+    return grid, line,
+
+anim = animation.FuncAnimation(fig, animate, 300, interval=50, init_func=init)
+plt.show()
+#anim.save("localization_2d.gif", writer="imagemagick")
